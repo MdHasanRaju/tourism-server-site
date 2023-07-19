35ef3570ea0cfe7e39562a0ae0d7b906eb531d40
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-07-19 10:21:53 +0600

    ::tada::tourism repaired ...

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cjag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cjag.mongodb.net/?retryWrites=true&w=majority`
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cjag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -64,7 +67,7 @@ async function run() {
     //  Deleted From My order Dashboard
      app.delete("/deleteService/:id", async(req, res) =>{
         const id = req.params.id;
-        const deletedItem = {_id: ObjectId(id)}
+        const deletedItem = {_id:new ObjectId(id)}
         const result = await myCollection.deleteOne(deletedItem);
         console.log(result);
         res.send(result)
