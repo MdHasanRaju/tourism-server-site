abe16806365e48f6534f27e12b33ec6c37eeb20b
MdHasanRaju <mdhasanraju2017@gmail.com>
2023-07-19 10:25:07 +0600

    tourism repaired again

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6cjag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const nam = 'shipon'
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
